22c66ca5a906052eae4e3f5263bb34ed86fd47cb
Alberto Jose Blandon Moreno <ialbertbla98@gmail.com>
2019-12-09 15:46:46 -0600

    Modelos

---
+++ FincaToma1/FincaToma1/Models/Empleado.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FincaToma1.Models
+{
+    public class Empleado
+    {
+        [Key]
+        public int IDE { get; set; }
+        public string NombreE { get; set; }
+        public string ApellidoE { get; set; }
+        public string TelefonoE { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime FechaE { get; set; }
+    }
+}
---
+++ FincaToma1/FincaToma1/Models/Ubicacion.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FincaToma1.Models
+{
+    public class Ubicacion
+    {
+        [Key]
+        public int IDU { get; set; }
+        public string NombreU { get; set; }
+    }
+}
